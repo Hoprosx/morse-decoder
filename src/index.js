9f662ce7cffcd82d0a3d398043afad9ce25b36fb
Kirill Ivanov <hoprosx@gmail.com>
2023-08-11 20:18:15 +0500

    feat: add decode func

--- src/index.js
+++ src/index.js
@@ -38,7 +38,48 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = ''
+    let count = 0
+    
+    let encodedSymbol = ''
+    for(i of expr){
+        encodedSymbol +=i
+        count++
+        if(count != 0 && count % 10 === 0){
+            count = 0
+
+            let twoDigit = ''
+            let morse = ''
+            for(let j = 0; j < 10; j++){
+                if(encodedSymbol[j] == '*'){
+                    result +=' '
+                    break
+                }
+                if([2,4,6,8].includes(j)){
+                    twoDigit = ''
+                    twoDigit +=encodedSymbol[j]
+                } else {
+                    twoDigit +=encodedSymbol[j]
+                    if(twoDigit.length == 2){
+                        if(twoDigit == '10'){
+                            morse +='.'
+                        }else if(twoDigit == '11'){
+                            morse +='-'
+                        }
+                    }
+                    
+                }
+             
+            }
+            if(morse){
+                result +=MORSE_TABLE[morse]
+                morse = ''
+            }
+            encodedSymbol = ''
+
+        }
+    }
+    return result
 }
 
 module.exports = {
